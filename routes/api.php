5453bf95de86f0c9d357c721aca6f3daac9c3610
Iason Argyrakis <jasonargirakis@gmail.com>
2022-02-02 15:57:16 +0200

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('/login', [App\Http\Controllers\Auth\ApiAuthController::class,"login"]);
+Route::post('/register', [\App\Http\Controllers\Auth\ApiAuthController::class,"register"]);
+Route::post('/logout', [\App\Http\Controllers\Auth\ApiAuthController::class,"logout"]);
+Route::get('/test', [\App\Http\Controllers\Auth\ApiAuthController::class,"test"]);
+
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+    Route::get('/test-safe', [\App\Http\Controllers\Auth\ApiAuthController::class,"test"]);
+});
